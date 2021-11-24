818478d306c36559f990176dc7a006bb79de19fb
mfahmi0304 <m.fahminuraziz93@gmail.com>
2021-11-25 01:48:24 +0700

    [UPDATE] Main Menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private Button button1;
+    [SerializeField] private Button button2;
+    [SerializeField] private Button button3;
+
+    private void Start()
+    {
+        button1.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(1);
+        });
+
+        button2.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(2);
+        });
+
+        button3.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(3);
+        });
+    }
+}
